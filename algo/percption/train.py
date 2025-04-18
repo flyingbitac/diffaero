b155ac299b8bb4e40ffcf7e8a6570cf17c3a952a
zxh0916 <zxh0916@126.com>
2025-04-18 13:48:22 +0800

    init perception

--- algo/percption/train.py
+++ algo/percption/train.py
@@ -9,13 +9,14 @@ from world.backbone import WorldModel
 def main(cfg:DictConfig):
     wm = WorldModel(cfg)
     # cfg.encoder.use_state = False
+    grid = torch.randn(16, 64, 4000)
     cfg.encoder.use_image = True
-    obs = torch.randn(16,64,3,64,64)
+    obs = torch.randn(16,64,1,16,9)
     state = torch.randn(16, 64, 10)
     action = torch.randn(16,64,3)
     rewards = torch.randn(16,64)
     terminals = torch.randn(16,64)
-    loss, metrics = wm.update(obs, state, action, rewards, terminals)
+    loss, metrics = wm.update(obs, state, action, rewards, terminals, grid)
     print(f"loss {loss}")
     print(f"metrics {metrics}")
    
