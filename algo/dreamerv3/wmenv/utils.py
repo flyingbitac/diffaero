00b33894cccea84b83d57010fc0a1ce327fcdab9
zxh0916 <zxh0916@126.com>
2025-06-25 15:20:37 +0800

    remove useless part

--- algo/dreamerv3/wmenv/utils.py
+++ algo/dreamerv3/wmenv/utils.py
@@ -1,72 +1,15 @@
 from collections import OrderedDict
 from dataclasses import dataclass
-from functools import partial
-from pathlib import Path
-import random
-from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+from typing import Any, Dict, List, Tuple
 
-from tensorboardX import SummaryWriter
-from omegaconf import OmegaConf
-import numpy as np
 import torch
 from torch import Tensor
 from torch.optim.lr_scheduler import LambdaLR
 import torch.nn as nn
 from torch.optim import AdamW
-import wandb
-
-class Logger():
-    def __init__(self,path) -> None:
-        self.writer = SummaryWriter(logdir=path,flush_secs=1)
-        self.tag_step = {}
-    
-    def log(self,tag,value):
-        if tag not in self.tag_step:
-            self.tag_step[tag] = 0
-        else:
-            self.tag_step[tag] +=1
-        if "video" in tag:
-            self.writer.add_video(tag,value,self.tag_step[tag],fps=4)
-        elif "images" in tag:
-            self.writer.add_images(tag,value,self.tag_step[tag])
-        elif "hist" in tag:
-            self.writer.add_histogram(tag,value,self.tag_step[tag])
-        else:
-            self.writer.add_scalar(tag,value,self.tag_step[tag])
-
-ATARI_100K_GAMES = [
-    "Alien",
-    "Amidar",
-    "Assault",
-    "Asterix",
-    "BankHeist",
-    "BattleZone",
-    "Boxing",
-    "Breakout",
-    "ChopperCommand",
-    "CrazyClimber",
-    "DemonAttack",
-    "Freeway",
-    "Frostbite",
-    "Gopher",
-    "Hero",
-    "Jamesbond",
-    "Kangaroo",
-    "Krull",
-    "KungFuMaster",
-    "MsPacman",
-    "Pong",
-    "PrivateEye",
-    "Qbert",
-    "RoadRunner",
-    "Seaquest",
-    "UpNDown",
-]
-
 
 Logs = List[Dict[str, float]]
 ComputeLossOutput = Tuple[Tensor, Dict[str, Any]]
-
 
 class StateDictMixin:
     def _init_fields(self) -> None:
@@ -106,27 +49,6 @@ class CommonTools(StateDictMixin):
 
     def set(self, name: str, value: Any):
         return setattr(self, name, value)
-
-
-def compute_classification_metrics(confusion_matrix: Tensor) -> Tuple[Tensor, Tensor, Tensor]:
-    num_classes = confusion_matrix.size(0)
-    precision = torch.zeros(num_classes)
-    recall = torch.zeros(num_classes)
-    f1_score = torch.zeros(num_classes)
-
-    for i in range(num_classes):
-        true_positive = confusion_matrix[i, i].item()
-        false_positive = confusion_matrix[:, i].sum().item() - true_positive
-        false_negative = confusion_matrix[i, :].sum().item() - true_positive
-
-        precision[i] = true_positive / (true_positive + false_positive) if (true_positive + false_positive) != 0 else 0
-        recall[i] = true_positive / (true_positive + false_negative) if (true_positive + false_negative) != 0 else 0
-        f1_score[i] = (
-            2 * (precision[i] * recall[i]) / (precision[i] + recall[i]) if (precision[i] + recall[i]) != 0 else 0
-        )
-
-    return precision, recall, f1_score
-
 
 def configure_opt(model: nn.Module, lr: float, weight_decay: float, eps: float, *blacklist_module_names: str) -> AdamW:
     """Credits to https://github.com/karpathy/minGPT"""
@@ -196,135 +118,6 @@ def init_lstm(model: nn.Module) -> None:
             p.data[(n // 4) : (n // 2)].fill_(1)
         elif "bias_hh" in name:
             p.data.fill_(0)
-
-
-def get_path_agent_ckpt(path_ckpt_dir: Union[str, Path], epoch: int, num_zeros: int = 5) -> Path:
-    d = Path(path_ckpt_dir) / "agent_versions"
-    if epoch >= 0:
-        return d / f"agent_epoch_{epoch:0{num_zeros}d}.pt"
-    else:
-        all_ = sorted(list(d.iterdir()))
-        assert len(all_) >= -epoch
-        return all_[epoch]
-
-
-def keep_agent_copies_every(
-    agent_sd: Dict[str, Any],
-    epoch: int,
-    path_ckpt_dir: Path,
-    every: int,
-    num_to_keep: Optional[int],
-) -> None:
-    assert every > 0
-    assert num_to_keep is None or num_to_keep > 0
-    get_path = partial(get_path_agent_ckpt, path_ckpt_dir)
-    get_path(0).parent.mkdir(parents=False, exist_ok=True)
-
-    # Save agent
-    save_with_backup(agent_sd, get_path(epoch))
-
-    # Clean oldest
-    if (num_to_keep is not None) and (epoch % every == 0):
-        get_path(max(0, epoch - num_to_keep * every)).unlink(missing_ok=True)
-
-    # Clean previous
-    if (epoch - 1) % every != 0:
-        get_path(max(0, epoch - 1)).unlink(missing_ok=True)
-
-
-def process_confusion_matrices_if_any_and_compute_classification_metrics(logs: Logs) -> None:
-    cm = [x.pop("confusion_matrix") for x in logs if "confusion_matrix" in x]
-    if len(cm) > 0:
-        confusion_matrices = {k: sum([d[k] for d in cm]) for k in cm[0]}  # accumulate confusion matrices
-        metrics = {}
-        for key, confusion_matrix in confusion_matrices.items():
-            precision, recall, f1_score = compute_classification_metrics(confusion_matrix)
-            metrics.update(
-                {
-                    **{f"classification_metrics/{key}_precision_class_{i}": v for i, v in enumerate(precision)},
-                    **{f"classification_metrics/{key}_recall_class_{i}": v for i, v in enumerate(recall)},
-                    **{f"classification_metrics/{key}_f1_score_class_{i}": v for i, v in enumerate(f1_score)},
-                }
-            )
-
-        logs.append(metrics)  # Append the obtained metrics to logs (in place)
-
-
-def prompt_atari_game():
-    for i, game in enumerate(ATARI_100K_GAMES):
-        print(f"{i:2d}: {game}")
-    while True:
-        x = input("\nEnter a number: ")
-        if not x.isdigit():
-            print("Invalid.")
-            continue
-        x = int(x)
-        if x < 0 or x > 25:
-            print("Invalid.")
-            continue
-        break
-    game = ATARI_100K_GAMES[x]
-    return game
-
-
-def prompt_run_name(game):
-    cfg_file = Path("config/trainer.yaml")
-    cfg_name = OmegaConf.load(cfg_file).wandb.name
-    suffix = f"-{cfg_name}" if cfg_name is not None else ""
-    name = game + suffix
-    name_ = input(f"Confirm run name by pressing Enter (or enter a new name): {name}\n")
-    if name_ != "":
-        name = name_
-    return name
-
-
-def save_info_for_import_script(epoch: int, run_name: str, path_ckpt_dir: Path) -> None:
-    info = {"epoch": epoch, "name": run_name}
-    save_with_backup(info, path_ckpt_dir / "info_for_import_script.pt")
-
-
-def save_with_backup(obj: Any, path: Path):
-    bk = path.with_suffix(".bk")
-    if path.is_file():
-        path.rename(bk)
-    torch.save(obj, path)
-    bk.unlink(missing_ok=True)
-
-
-def set_seed(seed: int) -> None:
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    random.seed(seed)
-
-
-def skip_if_run_is_over(func: Callable) -> Callable:
-    def inner(*args, **kwargs):
-        path_run_is_over = Path(".run_is_over")
-        if not path_run_is_over.is_file():
-            func(*args, **kwargs)
-            path_run_is_over.touch()
-        else:
-            print(f"Run is marked as finished. To unmark, remove '{str(path_run_is_over)}'.")
-
-    return inner
-
-
-def try_until_no_except(func: Callable) -> None:
-    while True:
-        try:
-            func()
-        except KeyboardInterrupt:
-            break
-        except Exception:
-            continue
-        else:
-            break
-
-
-def wandb_log(logs: Logs, epoch: int):
-    for d in logs:
-        wandb.log({"epoch": epoch, **d})
 
 class EMAScalar():
     def __init__(self, decay) -> None:
